11e32a89d5c482282ea5e1e6ec36826b45f7a02c
Álvaro Olivié <alvarolivie@gmail.com>
2025-11-27 12:43:14 +0000

    Updat test to cover advances situation

--- totals_test.go
+++ totals_test.go
@@ -22,4 +22,21 @@ func TestNewTotals(t *testing.T) {
 		assert.Equal(t, "19", *doc.TaxTotal[0].TaxSubtotal[0].TaxCategory.Percent)
 
 	})
+
+	t.Run("peppol-1-advance.json", func(t *testing.T) {
+		doc, err := testInvoiceFrom("peppol-1-advance.json")
+		require.NoError(t, err)
+
+		assert.Equal(t, "1620.00", doc.LegalMonetaryTotal.LineExtensionAmount.Value)
+		assert.Equal(t, "1620.00", doc.LegalMonetaryTotal.TaxExclusiveAmount.Value)
+		assert.Equal(t, "1960.20", doc.LegalMonetaryTotal.TaxInclusiveAmount.Value)
+		assert.NotNil(t, doc.LegalMonetaryTotal.PrepaidAmount)
+		assert.Equal(t, "196.02", doc.LegalMonetaryTotal.PrepaidAmount.Value)
+		assert.NotNil(t, doc.LegalMonetaryTotal.PayableAmount)
+		assert.Equal(t, "1764.18", doc.LegalMonetaryTotal.PayableAmount.Value)
+
+		assert.Equal(t, "340.20", doc.TaxTotal[0].TaxAmount.Value)
+		assert.Equal(t, "VAT", doc.TaxTotal[0].TaxSubtotal[0].TaxCategory.TaxScheme.ID)
+		assert.Equal(t, "21.0", *doc.TaxTotal[0].TaxSubtotal[0].TaxCategory.Percent)
+	})
 }
