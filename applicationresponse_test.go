0c2b442c5f621838f60132925d12448a219db0ab
CarlosMari <carlos.mari@invopop.com>
2026-06-15 09:00:49 +0000

    Aligning OIOUBL test coverage to golden fixtures and removing in-converter phive validation

--- applicationresponse_test.go
+++ applicationresponse_test.go
@@ -1,7 +1,6 @@
 package ubl_test
 
 import (
-	"context"
 	"encoding/json"
 	"os"
 	"path/filepath"
@@ -13,27 +12,13 @@ import (
 	"github.com/invopop/gobl/bill"
 	"github.com/invopop/gobl/cal"
 	"github.com/invopop/gobl/org"
-	"github.com/invopop/phive"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 )
 
 const oioublResponseDir = "oioubl21-response"
 
 func TestConvertToApplicationResponse(t *testing.T) {
-	var pc phive.ValidationServiceClient
-	if *validate {
-		conn, err := grpc.NewClient(
-			"127.0.0.1:9090",
-			grpc.WithTransportCredentials(insecure.NewCredentials()),
-		)
-		require.NoError(t, err)
-		defer conn.Close() //nolint:errcheck
-		pc = phive.NewValidationServiceClient(conn)
-	}
-
 	examples, err := filepath.Glob(filepath.Join(getConvertPath(), oioublResponseDir, jsonPattern))
 	require.NoError(t, err)
 	require.NotEmpty(t, examples, "no ApplicationResponse examples found")
@@ -55,18 +40,6 @@ func TestConvertToApplicationResponse(t *testing.T) {
 			outPath := filepath.Join(getConvertPath(), oioublResponseDir, "out", outName)
 			if *updateOut {
 				require.NoError(t, os.WriteFile(outPath, data, 0644))
-			}
-
-			if *validate {
-				vesid := ubl.ContextOIOUBL21.VESIDs.ApplicationResponse
-				resp, err := pc.ValidateXml(context.Background(), &phive.ValidateXmlRequest{
-					Vesid:      vesid,
-					XmlContent: data,
-				})
-				require.NoError(t, err)
-				results, err := json.MarshalIndent(resp.Results, "", "  ")
-				require.NoError(t, err)
-				require.True(t, resp.Success, "Generated XML should be valid for %s: %s", vesid, string(results))
 			}
 
 			output, err := os.ReadFile(outPath)
--- examples_test.go
+++ examples_test.go
@@ -2,7 +2,6 @@ package ubl_test
 
 import (
 	"bytes"
-	"context"
 	"encoding/json"
 	"flag"
 	"io"
@@ -15,11 +14,8 @@ import (
 	ubl "github.com/invopop/gobl.ubl"
 	"github.com/invopop/gobl/bill"
 	"github.com/invopop/gobl/uuid"
-	"github.com/invopop/phive"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 )
 
 const (
@@ -32,23 +28,7 @@ const (
 // updateOut is a flag that can be set to update example files
 var updateOut = flag.Bool("update", false, "Update the example files in test/data")
 
-// validate is a flag that enables Phive validation
-var validate = flag.Bool("validate", false, "Run Phive validation on generated XML")
-
 func TestConvertToInvoice(t *testing.T) {
-	var pc phive.ValidationServiceClient
-
-	// Only connect to Phive if validation is requested
-	if *validate {
-		conn, err := grpc.NewClient(
-			"127.0.0.1:9090",
-			grpc.WithTransportCredentials(insecure.NewCredentials()),
-		)
-		require.NoError(t, err)
-		defer conn.Close() //nolint:errcheck
-		pc = phive.NewValidationServiceClient(conn)
-	}
-
 	// Define contexts to test
 	contexts := []struct {
 		name    string
@@ -89,25 +69,6 @@ func TestConvertToInvoice(t *testing.T) {
 					if *updateOut {
 						err = os.WriteFile(outPath, data, 0644)
 						require.NoError(t, err)
-					}
-
-					// Run Phive validation if requested
-					if *validate {
-						// Determine VESID based on document type
-						env, err := loadTestEnvelopeFromPath(example)
-						require.NoError(t, err)
-						inv, ok := env.Extract().(*bill.Invoice)
-						require.True(t, ok, "Document should be an invoice")
-						vesid := ctx.context.GetVESID(inv)
-
-						resp, err := pc.ValidateXml(context.Background(), &phive.ValidateXmlRequest{
-							Vesid:      vesid,
-							XmlContent: data,
-						})
-						require.NoError(t, err)
-						results, err := json.MarshalIndent(resp.Results, "", "  ")
-						require.NoError(t, err)
-						require.True(t, resp.Success, "Generated XML should be valid for %s: %s", vesid, string(results))
 					}
 
 					output, err := os.ReadFile(outPath)
